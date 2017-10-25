fc0c0b27b2c600a092c908ec9a79d5a216e90295
aprilhjshin <31872545+aprilhjshin@users.noreply.github.com>
2017-10-25 12:11:18 -0700

    Add files via upload

--- Semester.cpp
+++ Semester.cpp
@@ -2,3 +2,31 @@
 //CSIS 137 Wed Night Class
 //Midterm Project
 //Step 4
+
+#include "Semester.h"
+#include <string>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+Semester::Semester(string name, Date start, Date end) 
+	:semesterName(name), startDate(start), endDate(end)
+{
+
+}
+
+ostream& operator<<(ostream& output, const Semester& info)
+{
+	cout << "Semester: " << info.semesterName << "(" << info.startDate << "-" << info.endDate << ")" << endl;
+	return output;
+}
+
+istream& operator>>(istream& input, Semester& info)
+{
+	input >> setw(8) >> info.semesterName;
+	input.ignore();
+	input >> setw(10) >> info.startDate;
+	input.ignore();
+	input >> setw(10) >> info.endDate;
+	return input;
+}
--- Semester.h
+++ Semester.h
@@ -3,3 +3,23 @@
 //Midterm Project
 //Step 4
 #pragma once
+#define SEMESTER_H
+#include "Date.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Semester
+{
+	friend ostream& operator<<(ostream&, const Semester&);
+	friend istream& operator>>(istream&, Semester&);
+
+	private:
+		string semesterName;
+		Date startDate;
+		Date endDate;
+
+	public:
+		Semester(string, Date, Date);
+
+};
